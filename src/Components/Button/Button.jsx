a4f8ca1af321c1421fb20a5a1c793cb8504c6ac7
devMachadoA <amachadoamaral2@gmail.com>
2023-12-08 11:47:39 +0000

    json in public

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Button.module.css'
-import { Link } from 'react-router-dom';
+
 
 export default function Button({onClick, label}) {
   return (
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import styles from './footer.module.css';
 
 function Footer() {
 
   return (
     <footer className={styles.footer}>
-      {/* <ul className={styles.socialList}>
-        <li className={styles.item}><a href="#"> <FaFacebook /></a></li>
-        <li className={styles.item}><a href="#"><FaInstagram /></a></li>
-        <li className={styles.item}><a href="#"><FaWhatsapp /></a></li>
-      </ul> */}
       <p className={styles.name}> Novembro/ Dezembro 2023</p>
     </footer>
   );
