aad430cf6521777f52697f5e28260e7039d08288
devMachadoA <amachadoamaral2@gmail.com>
2024-08-08 18:40:10 +0100

    layout

--- src/Components/Pages/ErrorPage.jsx
+++ src/Components/Pages/ErrorPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import Header from '../layout/Header/Header';
+import Footer from '../layout/Footer/Footer';
 
 import styles from './ErrorPage.module.css';
 
--- src/Components/layout/Header/Header.jsx
+++ src/Components/layout/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Header.module.css';
-import readingBible from '../../img/reading-bible.png'
+import readingBible from '../../../img/reading-bible.png'
 
 
 export default function Header() {
